9ac55bbe122fb1aaa9fa2f912f9570f70b9ddceb
dsmith <daniel.smith@srcentre.com.au>
2018-04-04 17:25:11 +1000

    Initial test setup

---
+++ tests/testthat/test-expect-exclusive.R
@@ -0,0 +1,24 @@
+library(dplyr)
+context("expect_exclusive")
+
+dat <- tribble(
+  ~a1, ~a2, ~a3, ~a4, ~a5, ~a6, ~a7, ~a8, ~a9, ~a10,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+  0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+  0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+  1, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  0, 0, 0, 0, 1, 0, 0, 0, 0, 1
+)
+
+test_that("Exclusive check works", {
+  expect_failure(expect_exclusive(vars(a10), vars(matches("^a[0-9]")), data = dat))
+  expect_success(expect_exclusive(vars(a10), vars(matches("^a[0-4]")), data = dat))
+  expect_warning(expect_exclusive(vars(a10), vars(matches("^a[2-4]")), data = dat))
+})
+
