23bd0e90547c9a86f45e954c9269189d29b275fc
Danny Smith <dsmith@srcentre.com.au>
2019-03-19 13:11:56 +1100

    Update def-tests.R
    * `parse_quosure` is soft deprectaed, change to `parse_quo`
    * Force all columns in definitions file to be character to avoid `parse_quo` errors
    * Remove `str_` function to avoid stringr dependency
    * Remove magrittr dependency

--- inst/project-testdat/def-tests.R
+++ inst/project-testdat/def-tests.R
@@ -1,10 +1,9 @@
 library(dplyr)
-library(magrittr)
 library(rlang)
 
 read_excel_allsheets <- function(filename) {
   sheets <- openxlsx::getSheetNames(filename)
-  x <- lapply(sheets, function(x) suppressWarnings(openxlsx::read.xlsx(filename, sheet = x, skipEmptyCols = FALSE) %>% tbl_df))
+  x <- lapply(sheets, function(x) suppressWarnings(openxlsx::read.xlsx(filename, sheet = x, skipEmptyCols = FALSE) %>% tbl_df %>% mutate_all(as.character)))
   x <- lapply(x, function(x) { names(x) <- gsub("\\.", " ", names(x)); x })
   names(x) <- sheets
   x
@@ -20,7 +19,8 @@ value_missing <- names(get_testdata())[!names(get_testdata()) %in% defs[["Value 
 if (length(value_missing) > 0) warning("Some variables do not appear in the `Value checks` list: ", paste0("`", value_missing, "`", collapse = ", "))
 
 # remove variables with no base check from list and
-defs[["Base checks"]] %<>%
+defs[["Base checks"]] <-
+  defs[["Base checks"]] %>%
   filter(!is.na(Base)) %>%
   mutate(`Missing Values` = ifelse(`Missing Values` %in% c(NA, ""), "NULL", `Missing Values`))
 
@@ -28,8 +28,8 @@ defs[["Base checks"]] %<>%
 
 test_that("Base checks", {
   for (i in seq_len(nrow(defs[["Base checks"]]))) {
-    expect_base(!!parse_quosure(defs[["Base checks"]][[i, "Variable"]]),
-                !!parse_quosure(defs[["Base checks"]][[i, "Base"]]),
+    expect_base(!!parse_quo(defs[["Base checks"]][[i, "Variable"]]),
+                !!parse_quo(defs[["Base checks"]][[i, "Base"]]),
                 c(getOption("testdat.miss"), eval(parse_expr(defs[["Base checks"]][[i, "Missing Values"]]))),
                 defs[["Base checks"]][[i, "Missing Valid"]] %in% TRUE)
   }
@@ -37,7 +37,7 @@ test_that("Base checks", {
 
 # test_that("All labelled variables have no unlabelled values", {
 #   for (i in names(get_testdata())[sapply(get_testdata(), is.labelled)]) {
-#     expect_values(!!parse_quosure(i),
+#     expect_values(!!parse_quo(i),
 #                   attr(get_testdata()[[i]], "labels"))
 #   }
 # })
@@ -46,32 +46,32 @@ test_that("Value checks", {
   for (i in seq_len(nrow(defs[["Value checks"]]))) {
     # Length test
     if (!is.na(defs[["Value checks"]][[i, "Max Length"]])) {
-      expect_func(!!parse_quosure(defs[["Value checks"]][[i, "Variable"]]),
+      expect_func(!!parse_quo(defs[["Value checks"]][[i, "Variable"]]),
                   chk_length,
                   args = list(defs[["Value checks"]][[i, "Max Length"]]))
     }
     # Pattern test
     if (!is.na(defs[["Value checks"]][[i, "Pattern"]])) {
-      expect_func(!!parse_quosure(defs[["Value checks"]][[i, "Variable"]]),
+      expect_func(!!parse_quo(defs[["Value checks"]][[i, "Variable"]]),
                   chk_pattern,
                   args = list(defs[["Value checks"]][[i, "Pattern"]]))
     }
     # Uniqueness test
     if (defs[["Value checks"]][[i, "Unique"]] %in% TRUE) {
-      expect_unique(vars(!!parse_quosure(defs[["Base checks"]][[i, "Variable"]])))
+      expect_unique(vars(!!parse_quo(defs[["Base checks"]][[i, "Variable"]])))
     }
     # Value checks
     values <- c()
     if (defs[["Value checks"]][[i, "Labels"]] %in% TRUE)
-      values %<>% c(attr(get_testdata()[[defs[["Value checks"]][[i, "Variable"]]]], "labels"))
+      values <- c(values, attr(get_testdata()[[defs[["Value checks"]][[i, "Variable"]]]], "labels"))
 
-    for (j in names(defs[["Value checks"]])[str_detect(names(defs[["Value checks"]]), "^Range ")]) {
+    for (j in names(defs[["Value checks"]])[grepl("^ Range ", names(defs[["Value checks"]]))]) {
       if (!is.na(defs[["Value checks"]][[i, j]]))
-        values %<>% c(eval(parse_expr(defs[["Value checks"]][[i, j]])))
+        values <- c(values, eval(parse_expr(defs[["Value checks"]][[i, j]])))
     }
 
     if (length(values) > 0) {
-      expect_values(!!parse_quosure(defs[["Value checks"]][[i, "Variable"]]),
+      expect_values(!!parse_quo(defs[["Value checks"]][[i, "Variable"]]),
                     values)
     }
   }
@@ -79,8 +79,8 @@ test_that("Value checks", {
 
 test_that("Consistency checks", {
   for (i in seq_len(nrow(defs[["Consistency checks"]]))) {
-    expect_cond(!!parse_quosure(defs[["Consistency checks"]][[i, "Condition 1"]]),
-                !!parse_quosure(defs[["Consistency checks"]][[i, "Condition 2"]]))
+    expect_cond(!!parse_quo(defs[["Consistency checks"]][[i, "Condition 1"]]),
+                !!parse_quo(defs[["Consistency checks"]][[i, "Condition 2"]]))
   }
 })
 
