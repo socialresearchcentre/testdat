97fa3741de84db2e068dcba3715941851a0eab8e
Danny Smith <dsmith@srcentre.com.au>
2019-04-16 14:59:51 +1000

    Cleaning up
    * use_tidy_description
    * Add license
    * Ignore inst/doc

--- inst/doc/testdat.R
+++
@@ -1,5 +0,0 @@
-## ---- echo = FALSE, warning = FALSE, message = FALSE---------------------
-knitr::opts_chunk$set(collapse = T, comment = "#>", error = T)
-options(tibble.print_min = 4L, tibble.print_max = 4L)
-library(testdat)
-
