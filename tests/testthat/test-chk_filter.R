e74d01fa13221e7e25444d00639d7ac09e69ee13
kinto-b <kinto.behr@srcentre.com.au>
2021-01-18 17:04:31 +1100

    Add tests for `chk_filter` functions (#4)

---
+++ tests/testthat/test-chk_filter.R
@@ -0,0 +1,23 @@
+test_that("chk_filter", {
+  test_df <- data.frame(
+    a = 1:10,
+    b = rep(0:1, 5) * 2,
+    c = c(runif(5, max = 1), runif(5, max = 100))
+  )
+
+  expect_identical(
+    chk_filter(test_df, c, chk_range, a <= 5, list(min = 0, max = 1)),
+    c(rep(TRUE, 5), rep(NA, 5))
+  )
+
+  expect_identical(
+    chk_filter_all(test_df, c("b", "c"), chk_range, a <= 5, list(min = 0, max = 1)),
+    c(!as.logical(test_df$b[1:5]), rep(NA, 5))
+  )
+
+  expect_identical(
+    chk_filter_any(test_df, c("b", "c"), chk_range, a <= 5, list(min = 0, max = 1)),
+    c(rep(TRUE, 5), rep(NA, 5))
+  )
+
+})
