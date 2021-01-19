61536c06a30bb3bfdde51f7bbb1cb441e2e865de
kinto-b <kinto.behr@srcentre.com.au>
2021-01-19 12:02:48 +1100

    Add tests for `expect-generic` functions (#4)

---
+++ tests/testthat/test-expect_generic.R
@@ -0,0 +1,8 @@
+
+test_that("basic examples", {
+  expect_success(expect_all(c("vs", "am"), chk_values, args = list(0:1), data = mtcars))
+  expect_failure(expect_all(c("vs", "carb"), chk_values, args = list(0:1), data = mtcars))
+
+  expect_success(expect_any(c("vs", "carb"), chk_values, args = list(0:1), data = mtcars))
+  expect_failure(expect_any(c("cyl", "carb"), chk_values, args = list(0:1), data = mtcars))
+})
