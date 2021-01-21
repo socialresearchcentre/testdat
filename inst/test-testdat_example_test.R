ce847c5d7ee05e6b77feb4753b7b1ff8a32511c3
kinto-b <kinto.behr@srcentre.com.au>
2021-01-21 15:57:30 +1100

    Add tests for `reporter_excel` functions (#4)

---
+++ inst/test-testdat_example_test.R
@@ -0,0 +1,28 @@
+test_that("passes", {
+  context("passing_tests")
+  library(testdat)
+  mydf <- data.frame(
+    my_unique = 1:10,
+    my_binary = sample(0:1, 10, replace = TRUE),
+    my_string = rownames(mtcars)[1:10],
+    my_real = 1:10 / 11:20
+  )
+
+  expect_values(my_binary, 0:1, data = mydf)
+  expect_unique(vars(my_unique), data = mydf)
+  expect_range(my_real, 0, 0.5, data = mydf)
+})
+
+test_that("fails", {
+  context("failing_tests")
+  library(testdat)
+  mydf <- data.frame(
+    my_unique = 1:10,
+    my_binary = sample(0:1, 10, replace = TRUE),
+    my_string = rownames(mtcars)[1:10],
+    my_real = 1:10 / 11:20
+  )
+
+  expect_regex(my_string, "Merc", data = mydf)
+  expect_range(my_real, 0, 0.4, data = mydf)
+})
---
+++ tests/testthat/test-reporter_excel.R
@@ -0,0 +1,45 @@
+test_that("excel_results", {
+  my_fp <- system.file("test-testdat_example_test.R", package = "testdat")
+  x <- test_file(my_fp, reporter = SilentReporter)
+  x_summ <- summarise_results_excel(x)
+
+  # Check that summary looks right
+  expect_s3_class(x_summ, "tbl_df")
+  expect_success(expect_values(status, "success", data = x_summ[x_summ$test == "passes", ]))
+  expect_success(expect_values(status, "failure", data = x_summ[x_summ$test == "fails", ]))
+
+  file_out <- paste0("temp_", rlang::hash("temp"), ".xlsx")
+  output_results_excel(x, file_out)
+  x_xl_summary <- openxlsx::read.xlsx(file_out, "__Summary")
+  x_xl_passing <- openxlsx::read.xlsx(file_out, "passing_tests")
+  x_xl_failing <- openxlsx::read.xlsx(file_out, "failing_tests")
+  file.remove(file_out)
+
+  # Check that .xslx file looks good
+  xl_summary <- data.frame(
+    context = c(NA_real_, NA_real_),
+    tests = c(2L, 3L),
+    failed = c(2L, 0L),
+    error = c(0L, 0L),
+    skipped = c(0L, 0L),
+    warning = c(0L, 0L)
+  )
+
+  xl_failing <- data.frame(
+    stringsAsFactors = FALSE,
+    context = c("failing_tests", "failing_tests"),
+    test = c("fails", "fails"),
+    status = c("failure", "failure"),
+    variable = c("my_string", "my_real"),
+    description = c("`mydf` has 7 records failing pattern check on variable `my_string`. Filter: None Arguments: `<named list>`",
+                    "`mydf` has 4 records failing range check on variable `my_real`. Filter: None Arguments: `<named list>`"),
+    failed_records = c(7L, 4L),
+    total_records = c(10L, 10L),
+    call = c("NULL", "NULL")
+  )
+
+  expect_equal(x_xl_summary, xl_summary)
+  expect_equal(x_xl_failing, xl_failing)
+  expect_equal(names(x_xl_passing), names(xl_failing))
+  expect_equal(nrow(x_xl_passing), 0)
+})
