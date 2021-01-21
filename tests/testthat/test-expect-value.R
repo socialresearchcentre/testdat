9d34856118bdec34a5d1d6e174f651c87790164c
kinto-b <kinto.behr@srcentre.com.au>
2021-01-21 15:57:12 +1100

    Add tests for `expect-value` functions (#4)

---
+++ tests/testthat/test-expect-value.R
@@ -0,0 +1,27 @@
+test_that("expect_values", {
+  # We can check against expect_make
+  my_expect_values <- expect_make(chk_values)
+  for (i in 1:25) {
+    x <- data.frame(key = sample(1:3, 2, replace = TRUE))
+    my_res <- testthat::capture_error(my_expect_values(key, 1:2, data = x))
+    pkg_res <- testthat::capture_error(expect_values(key, 1:2, data = x))
+    expect_identical(class(my_res), class(pkg_res)) # Either both should be an error or both should be NULL
+  }
+})
+
+test_that("expect_unique", {
+  # We can check against expect_make
+  my_expect_unique <- expect_make(chk_unique, vars = TRUE)
+  for (i in 1:25) {
+    x <- data.frame(key = sample(1:3, 3, replace = TRUE))
+    my_res <- testthat::capture_error(my_expect_unique(vars(key), data = x))
+    pkg_res <- testthat::capture_error(expect_unique(vars(key), data = x))
+    expect_identical(class(my_res), class(pkg_res)) # Either both should be an error or both should be NULL
+  }
+})
+
+test_that("expect_unique_across", {
+  df1 <- data.frame(a = 1:10, b = 99, c = 1)
+  expect_success(expect_unique_across(vars(a,b), data = df1))
+  expect_failure(expect_unique_across(vars(a,c), data = df1))
+})
