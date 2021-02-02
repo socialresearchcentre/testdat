85f4c849bcd1cc2194c2984b81b46ae01b722877
kinto-b <kinto.behr@srcentre.com.au>
2021-02-02 12:40:06 +1100

    Implement `expect_where` (closes #8)

--- R/expect-generic.R
+++ R/expect-generic.R
@@ -7,7 +7,9 @@
 #' applying `func` to each of the `vars` is TRUE). The latter, `expect_any`,
 #' tests the `vars` to see whether `func` returns TRUE for *any* of them (i.e.
 #' whether the disjunction of the results of applying `func` to each of the `vars`
-#' is TRUE).
+#' is TRUE). The `expect_where` function works exactly like `expect_all` except
+#' that variables are specified not using `dplyr::vars()` (`vars`) but using
+#' bare [`tidy-select`][dplyr_tidy_select] functions (`where`).
 #'
 #' @inheritParams data-params
 #' @param func a function that takes a vector as the first argument and returns
@@ -20,25 +22,34 @@
 #' @param ... arguments to pass to `expect_allany()`
 #' @examples
 #' \dontrun{
-#' set_testdata(mtcars)
 #' # Check that every 4-cylinder car has an engine displacement of < 100 cubic
 #' # inches *AND* < 100 horsepower
 #' expect_all(
-#'   vars = c("disp", "hp"),
+#'   vars = vars(disp, hp),
 #'   func = chk_range,
 #'   flt = (cyl == 4),
 #'   args = list(min = 0, max = 100),
+#'   data = mtcars
 #' )
 #'
 #' # Check that every 4-cylinder car has an engine displacement of < 100 cubic
 #' # inches *OR* < 100 horsepower
 #' expect_any(
-#'   vars = c("disp", "hp"),
+#'   vars = vars(disp, hp),
 #'   func = chk_range,
 #'   flt = (cyl == 4),
 #'   args = list(min = 0, max = 100),
+#'   data = mtcars
 #' )
 #' }
+#'
+#' # Check petal dimensions are positive
+#' expect_where(
+#'   where = where(is.numeric),
+#'   func = chk_range,
+#'   args = list(min = 0, max = Inf),
+#'   data = iris
+#' )
 #'
 #' @seealso [chk-generic] for a set of generic checking functions
 #' @family data expectations
@@ -89,4 +100,37 @@ expect_all <- function(...) {
 #' @rdname generic-expectations
 expect_any <- function(...) {
   expect_allany(..., allany = chk_filter_any)
+}
+
+#' @export
+#' @param where <[`tidy-select`][dplyr_tidy_select]> columns to check
+#' @rdname generic-expectations
+expect_where <- function(where, func, flt = TRUE, data = get_testdata(), args = list(), func_desc = NULL) {
+  where <- enquo(where)
+
+  act <- quasi_label(enquo(data))
+  act$func_desc <- if (is.null(func_desc)) paste0("`", as_label(enquo(func)), "`") else func_desc
+  act$var_desc  <- data %>% select(!!where) %>% names() %>% paste(collapse = ", ")
+  act$flt_desc  <- as_label_flt(enquo(flt))
+  act$args_desc <- as_label_repl(args, "(^list\\()|(\\)$)", "")
+
+  act$result <- chk_filter_where(eval_tidy(enquo(data)),
+                                 !!where,
+                                 eval_tidy(enquo(func)),
+                                 !!enquo(flt),
+                                 args)
+
+  expect_custom(
+    all(act$result, na.rm = TRUE),
+    glue("{act$lab} has {sum(!act$result, na.rm = TRUE)} records failing \\
+          {act$func_desc} on variable `{act$var_desc}`.
+          Filter: {act$flt_desc}
+          Arguments: `{act$args_desc}`"),
+    failed_count = sum(!act$result, na.rm = TRUE),
+    total_count = sum(!is.na(act$result)),
+    var_desc = act$var_desc,
+    result = act$result
+  )
+
+  invisible(act$result)
 }
--- tests/testthat/test-expect_generic.R
+++ tests/testthat/test-expect_generic.R
@@ -1,8 +1,20 @@
 
 test_that("basic examples", {
-  expect_success(expect_all(c("vs", "am"), chk_values, args = list(0:1), data = mtcars))
-  expect_failure(expect_all(c("vs", "carb"), chk_values, args = list(0:1), data = mtcars))
+  expect_success(expect_all(vars(vs, am), chk_values, args = list(0:1), data = mtcars))
+  expect_failure(expect_all(vars(vs, carb), chk_values, args = list(0:1), data = mtcars))
 
-  expect_success(expect_any(c("vs", "carb"), chk_values, args = list(0:1), data = mtcars))
-  expect_failure(expect_any(c("cyl", "carb"), chk_values, args = list(0:1), data = mtcars))
+  expect_success(expect_any(vars(vs, carb), chk_values, args = list(0:1), data = mtcars))
+  expect_failure(expect_any(vars(cyl, carb), chk_values, args = list(0:1), data = mtcars))
+})
+
+
+test_that("expect_where", {
+  expect_identical(
+    capture_expectation(expect_all(vars(vs, am), chk_values, args = list(0:1), data = mtcars)),
+    capture_expectation(expect_where(where(function(x) all(x %in% 0:1)), chk_values, args = list(0:1), data = mtcars)),
+  )
+  expect_identical(
+    capture_expectation(expect_all(vars(qsec), chk_range, flt = mpg < 25, args = list(min = 18, max = Inf), data = mtcars)),
+    capture_expectation(expect_where(all_of("qsec"), chk_range, flt = mpg < 25, args = list(min = 18, max = Inf), data = mtcars)),
+  )
 })
