bcf6934aebb9b4de5f7a9b84cd69edff40fdf28a
kinto-b <kinto.behr@srcentre.com.au>
2021-02-09 10:07:30 +1100

    Reorganise value expectations (#4)
    * Uniqueness expectations and exclusivity expectations are separated from other value expectations and placed in their own scripts

---
+++ R/expect-unique.R
@@ -0,0 +1,124 @@
+#' Expectations: uniqueness checks
+#'
+#' These functions test variables for uniqueness:
+#'   * `expect_unique` tests a set of variables (`vars`) and fails if the
+#'     variables collectively do not uniquely identify the observations
+#'   * `expect_unique_across` tests a set of variables (`vars`) and fails if
+#'     each observation does not have different values in each of the variables
+#'     in `vars`.
+#'   * `expect_unique_combine` tests a set of variables (`vars`) and fails if
+#'     any values appear more than once across all of them.
+#'
+#'
+#' @inheritParams data-params
+#' @family data expectations
+#' @name uniqueness-expectations
+#' @examples
+#'
+#' student_fruit_preferences <- data.frame(
+#'   student_id = 1:5,
+#'   apple = c(1, 1, 1, 1, 1),
+#'   orange = c(2, 3, 2, 3, 2),
+#'   banana = c(3, 2, 3, 2, 3),
+#'   phone1 = c(123, 456, 789, 987, 654),
+#'   phone2 = c(345, 678, 987, 567, 000)
+#' )
+#'
+#' #' # Check that key is unique
+#' expect_unique(vars(student_id), data = student_fruit_preferences)
+#'
+#' # Check that every observation has a different value across a set of variables
+#' expect_unique_across(vars(apple, orange, banana), data = student_fruit_preferences)
+#'
+#' # Check that each phone number appears at most once
+#' \dontrun{expect_unique_combine(vars(phone1, phone2), data = student_fruit_preferences)}
+NULL
+
+#' @importFrom tidyselect vars_select
+#' @export
+#' @rdname uniqueness-expectations
+expect_unique <- function(vars, flt = TRUE, data = get_testdata()) {
+  act <- quasi_label(enquo(data))
+  act$var_desc <- as_label_vars(enquo(vars))
+  act$flt_desc <- as_label_flt(enquo(flt))
+
+  flt <- enquo(flt)
+  act$result_data <- data %>%
+    filter(!!flt) %>%
+    group_by(!!!vars) %>%
+    mutate(count = n()) %>%
+    ungroup() %>%
+    select(!!!vars, count)
+  # select(!!!vars) %>%
+  # duplicated %>%
+  # `!`
+
+  act$result <- act$result_data$count == 1
+
+  expect_custom(
+    all(act$result, na.rm = TRUE),
+    glue("{act$lab} has {sum(!act$result, na.rm = TRUE)} duplicate records \\
+          on variable `{act$var_desc}`.
+          Filter: {act$flt_desc}"),
+    failed_count = sum(!act$result, na.rm = TRUE),
+    total_count = sum(!is.na(act$result)),
+    duplicated_ids = act$result_data %>% filter(count > 1) %>% unique
+  )
+
+  invisible(act$result)
+}
+
+#' @export
+#' @rdname uniqueness-expectations
+expect_unique_across <- function(vars, flt = TRUE, data = get_testdata()) {
+  act <- quasi_label(enquo(data))
+  act$var_desc <- as_label_vars(enquo(vars))
+  act$flt_desc <- as_label_flt(enquo(flt))
+
+  flt <- enquo(flt)
+  act$result <- data %>%
+    filter(!!flt) %>%
+    select(!!!vars) %>%
+    apply(1, function(x) { all(chk_unique(x)) })
+
+  expect_custom(
+    all(act$result, na.rm = TRUE),
+    glue("{act$lab} has {sum(!act$result, na.rm = TRUE)} records with \\
+          duplicates across variables `{act$var_desc}`.
+          Filter: {act$flt_desc}"),
+    failed_count = sum(!act$result, na.rm = TRUE),
+    total_count = sum(!is.na(act$result))
+  )
+
+  invisible(act$result)
+}
+
+#' @export
+#' @rdname uniqueness-expectations
+expect_unique_combine <- function(vars, flt = TRUE, data = get_testdata()) {
+  act <- quasi_label(enquo(data))
+  act$var_desc <- as_label_vars(enquo(vars))
+  act$flt_desc <- as_label_flt(enquo(flt))
+
+  flt <- enquo(flt)
+  test_data <- data %>%
+    filter(!!flt) %>%
+    select(!!!vars)
+
+  all_values <- unlist(lapply(test_data, function(x) as.character(x)))
+  duplicate_values <- all_values[duplicated(all_values)]
+
+  act$result <- test_data %>%
+    filter(if_any(everything(), ~ .x %in% duplicate_values))
+
+  expect_custom(
+    nrow(act$result) == 0,
+    glue("{act$lab} has {nrow(act$result)} records with duplicate values \\
+           across variables `{act$var_desc}`.
+           Filter: {act$flt_desc}"),
+    failed_count = nrow(act$result),
+    total_count = nrow(test_data)
+  )
+
+  invisible(act$result)
+}
--- tests/testthat/test-expect-value.R
+++ tests/testthat/test-expect-value.R
@@ -9,32 +9,4 @@ test_that("expect_values", {
   }
 })
 
-test_that("expect_unique", {
-  # We can check against expect_make
-  my_expect_unique <- expect_make(chk_unique, vars = TRUE)
-  for (i in 1:25) {
-    x <- data.frame(key = sample(1:3, 3, replace = TRUE))
-    my_res <- testthat::capture_error(my_expect_unique(vars(key), data = x))
-    pkg_res <- testthat::capture_error(expect_unique(vars(key), data = x))
-    expect_identical(class(my_res), class(pkg_res)) # Either both should be an error or both should be NULL
-  }
-})
 
-test_that("expect_unique_across", {
-  df1 <- data.frame(a = 1:10, b = 99, c = 1)
-  expect_success(expect_unique_across(vars(a,b), data = df1))
-  expect_failure(expect_unique_across(vars(a,c), data = df1))
-})
-
-
-test_that("expect_unique_combine", {
-  df1 <- data.frame(
-    a = 1:10,
-    b = 11:20,
-    c = as.character(c(1:3, 14:16, 27:30)),
-    e = rep(1, 10)
-  )
-  expect_success(expect_unique_combine(vars(a, b), data = df1))
-  expect_failure(expect_unique_combine(vars(a, b, c), data = df1))
-  expect_failure(expect_unique_combine(vars(e), data = df1))
-})
