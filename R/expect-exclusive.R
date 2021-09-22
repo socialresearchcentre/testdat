90388d05640f815c582d885d2dd74c6bc907faf4
Danny Smith <danny@gorcha.org>
2021-09-22 21:14:47 +1000

    Update `expect_exclusive()`: * use tidyselect * rename argument `exc_vars` to `var_set` * rewrite documentation

--- R/expect-exclusive.R
+++ R/expect-exclusive.R
@@ -1,25 +1,35 @@
 #' Expectations: exclusivity checks
 #'
-#' `expect_exclusive` tests one set of variables (`exc_vars`) against another
-#' (`vars`) with reference to a value (`exc_val`) and fails if any observation
-#' has *all* of the variables in `vars` equal to `exc_val` and and *any* of the
-#' variables in `exc_vars` (aside from those which are also in `vars`) equal to
-#' `exc_val`.
+#' `expect_exclusive` tests that `vars` are exclusive - that, if any one of
+#' `vars` is set to `exc_val`, no other variable in `vars` or `var_set` is also
+#' set to `exc_val`.
 #'
+#' This expectation is designed to check exclusivity in survey multiple response
+#' sets, where one response is only valid on its own.
+#'
+#' See the example data set below:
+#' * no record should have `q10_98`, "None of the above", selected while also
+#' having any other response selected, so we refer to this an "exclusive"
+#' response.
+#' * `expect_exclusive()` checks whether `q10_98` "None of the above" or
+#' `q10_99` "Don't know", the exclusive responses, have been selected alongside
+#' any other `q10_*` response.
+#' * The expectations fails, since the first record has both `q10_1` and
+#' `q10_98` selected.
 #'
 #' @inheritParams data-params
-#' @param exc_vars The variable set to check, specified using [vars()]. This
+#' @param var_set The full variable set to check, specified using [vars()]. This
 #'   should include all variables specified in the `vars` argument
-#' @param exc_val The value to check for exclusivity (default: `1`)
+#' @param exc_val The value that flags a variable as "selected" (default: `1`)
 #' @family data expectations
 #' @name exclusivity-expectations
 #' @examples
 #'
 #' my_q_block <- data.frame(
 #'   resp_id = 1:5, # Unique to respondent
-#'   q10_1 = c(1, 1, 0, 0, 0),
-#'   q10_2 = c(0, 1, 0, 0, 0),
-#'   q10_3 = c(0, 0, 1, 0, 0),
+#'   q10_1  = c(1, 1, 0, 0, 0),
+#'   q10_2  = c(0, 1, 0, 0, 0),
+#'   q10_3  = c(0, 0, 1, 0, 0),
 #'   q10_98 = c(1, 0, 0, 1, 0), # None of the above
 #'   q10_99 = c(0, 0, 0, 0, 1)  # Item not answered
 #' )
@@ -28,54 +38,60 @@
 #' # none of the other question options are selected:
 #' try(
 #' expect_exclusive(
-#'   vars(q10_98, q10_99),
-#'   vars(starts_with("q10_")),
+#'   c(q10_98, q10_99),
+#'   starts_with("q10_"),
 #'   data = my_q_block
 #' )
 #' )
 NULL
 
+#' @importFrom tidyselect eval_select
 #' @export
 #' @rdname exclusivity-expectations
-expect_exclusive <- function(vars, exc_vars, exc_val = 1, flt = TRUE, data = get_testdata()) {
+expect_exclusive <- function(vars, var_set, exc_val = 1, flt = TRUE, data = get_testdata()) {
   act <- quasi_label(enquo(data))
   act$var_desc     <- as_label_vars(enquo(vars))
-  act$exc_var_desc <- as_label_vars(enquo(exc_vars))
+  act$var_set_desc <- as_label_vars(enquo(var_set))
   act$exc_val_desc <- as_label(enquo(exc_val))
   act$flt_desc     <- as_label_flt(enquo(flt))
 
-  vars_list <- vars_select(tbl_vars(data), !!!vars) %>% unname
-  exc_list <- vars_select(tbl_vars(data), !!!exc_vars) %>% unname
+  vars_list <- names(data)[
+    eval_select(enquo(vars), data, allow_rename = FALSE)
+  ]
+  exc_list <- names(data)[
+    eval_select(enquo(var_set), data, allow_rename = FALSE)
+  ]
 
   if (any(!vars_list %in% exc_list)) {
-    exc_vars <- c(exc_vars, vars)
-    warning("Some variables in `vars(", act$var_desc, ")` do not exist in ",
-            "exc_vars `vars(", act$exc_var_desc, ")`. ",
+    warning("Some variables in `", act$var_desc, "` do not exist in ",
+            "var_set `", act$var_set_desc, "`. ",
             "Variable lists will be combined.",
             call. = FALSE)
   }
 
   flt <- enquo(flt)
   act$result <-
-    # Flags records with no codes or 1 code selected from all vars
+    !(
+    # Flags records that have the exclusive var selected
     (data %>%
        filter(!!flt) %>%
-       mutate_at(exc_vars, ~. %==% exc_val) %>%
-       select(!!!exc_vars) %>%
+       mutate(across({{ vars }}, ~. %==% exc_val)) %>%
+       select({{ vars }}) %>%
+       apply(1, any)) &
+    # Flags records with more than one var selected
+    (data %>%
+       filter(!!flt) %>%
+       mutate(across(c({{ var_set }}, {{ vars }}), ~. %==% exc_val)) %>%
+       select(c({{ var_set }}, {{ vars }})) %>%
        apply(1, sum) %>%
-       `%in%`(c(0, 1))) |
-    # Flags codes that do NOT have the exclusive var selected
-    (data %>%
-       filter(!!flt) %>%
-       mutate_at(vars, ~. %!=% exc_val) %>%
-       select(!!!vars) %>%
-       apply(1, all))
+       `>`(1))
+    )
 
   expect_custom(
     all(act$result, na.rm = TRUE),
     glue("{act$lab} has {sum(!act$result, na.rm = TRUE)} records with \\
           non-exclusive values in variables `{act$var_desc}` in variable set \\
-          `{act$exc_var_desc}`.
+          `{act$var_set_desc}`.
           Filter: {act$flt_desc}"),
     failed_count = sum(!act$result, na.rm = TRUE),
     total_count = sum(!is.na(act$result))
